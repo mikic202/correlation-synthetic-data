cdf9c96daf4f198cf715786db5ecbcd391cee752
mikic202 <mikolaj.chomanski@gmail.com>
2025-07-14 19:50:28 +0200

    Add measuring auc with tabpfn

---
+++ performance_metrics/measure_tabpfn_auc.py
@@ -0,0 +1,18 @@
+from tabpfn import TabPFNClassifier
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+
+
+def measure_tabpfn_auc(
+    synthetic_x: list[pd.DataFrame],
+    synthetic_y: list[list[int]],
+    reral_x: pd.DataFrame,
+    real_y: list[int],
+):
+    areas_under_curve = []
+    for synt_x, synth_y in zip(synthetic_x, synthetic_y):
+        random_forest_clasifier = TabPFNClassifier().fit(synt_x, synth_y)
+        areas_under_curve.append(
+            roc_auc_score(real_y, random_forest_clasifier.predict_proba(reral_x)[:, 1])
+        )
+    return areas_under_curve
