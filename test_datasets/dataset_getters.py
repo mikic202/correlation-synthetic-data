82f77de7dfa8420f635ce5f6fab864e84c2358c9
mikic202 <mikolaj.chomanski@gmail.com>
2025-07-15 21:25:53 +0200

    Add 4 regresion datasets

--- test_datasets/dataset_getters.py
+++ test_datasets/dataset_getters.py
@@ -47,3 +47,92 @@ def get_analcatdata_authorship_dataset(
     dataset["Author"] = dataset["Author"].factorize()[0]
     dataset = dataset.rename(columns={"Author": "c"}).astype("float32")
     return train_test_split(dataset, test_size=test_size)
+
+
+def get_heart_failure_clinical_regresion_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(46612).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.rename(columns={"platelets": "target"}).astype("float32")
+    return train_test_split(dataset, test_size=test_size)
+
+
+def get_superconduct_regression_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(44148).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.rename(columns={"criticaltemp": "target"}).astype("float32")
+    return train_test_split(dataset, test_size=test_size)
+
+
+def get_sleep_deprivation_and_cognitive_performance_regression_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(46754).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.drop(columns=["Participant_ID"], axis=1)
+    dataset["Gender"] = dataset["Gender"].factorize()[0]
+    dataset = dataset.rename(columns={"Stress_Level": "target"})
+    return train_test_split(dataset, test_size=test_size)
+
+
+def get_house_prices_regression_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(42563).get_data(
+        dataset_format="dataframe"
+    )
+    nominal_features = [
+        "MSZoning",
+        "Street",
+        "Alley",
+        "LotShape",
+        "LandContour",
+        "Utilities",
+        "LotConfig",
+        "LandSlope",
+        "Neighborhood",
+        "Condition1",
+        "Condition2",
+        "BldgType",
+        "HouseStyle",
+        "RoofStyle",
+        "RoofMatl",
+        "Exterior1st",
+        "Exterior2nd",
+        "MasVnrType",
+        "ExterQual",
+        "ExterCond",
+        "Foundation",
+        "BsmtQual",
+        "BsmtCond",
+        "BsmtExposure",
+        "BsmtFinType1",
+        "BsmtFinType2",
+        "Heating",
+        "HeatingQC",
+        "CentralAir",
+        "Electrical",
+        "KitchenQual",
+        "Functional",
+        "FireplaceQu",
+        "GarageType",
+        "GarageFinish",
+        "GarageQual",
+        "GarageCond",
+        "PavedDrive",
+        "PoolQC",
+        "Fence",
+        "MiscFeature",
+        "SaleType",
+        "SaleCondition",
+    ]
+    for feature_to_factorize in nominal_features:
+        dataset[feature_to_factorize] = dataset[feature_to_factorize].factorize()[0]
+    dataset = dataset.rename(columns={"SalePrice": "target"})
+    return train_test_split(dataset, test_size=test_size)
