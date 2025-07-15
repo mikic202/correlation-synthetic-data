16d4c04430d6016a7e1032c3b75ed9df89f79ca1
mikic202 <mikolaj.chomanski@gmail.com>
2025-07-15 20:12:24 +0200

    Add more clasification datasets

--- test_datasets/dataset_getters.py
+++ test_datasets/dataset_getters.py
@@ -18,3 +18,31 @@ def get_mfeat_zernike_dataset(
     )
     dataset = dataset.rename(columns={"class": "c"}).astype("float32")
     return train_test_split(dataset, test_size=test_size)
+
+
+def get_climate_model_simulation_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(40994).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.rename(columns={"outcome": "c"})
+    return train_test_split(dataset, test_size=test_size)
+
+
+def get_wdbc_dataset(test_size: float = 0.1) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(1510).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.rename(columns={"Class": "c"})
+    return train_test_split(dataset, test_size=test_size)
+
+
+def get_analcatdata_authorship_dataset(
+    test_size: float = 0.1,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset, _, _, _ = openml.datasets.get_dataset(458).get_data(
+        dataset_format="dataframe"
+    )
+    dataset = dataset.rename(columns={"Author": "c"})
+    return train_test_split(dataset, test_size=test_size)
