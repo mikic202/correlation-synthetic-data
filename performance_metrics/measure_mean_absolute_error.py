83c02c25da152760a8d9b2718ae1e129678106d5
mikic202 <mikolaj.chomanski@gmail.com>
2025-07-18 12:11:11 +0200

    Add TabpFN and xgboost regresion analyzis

--- performance_metrics/measure_mean_absolute_error.py
+++ performance_metrics/measure_mean_absolute_error.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from tabpfn import TabPFNRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
 
 
@@ -11,7 +13,39 @@ def measure_random_forest_mean_absolute_error(
 ):
     mean_absolute_errors = []
     for synt_x, synth_y in zip(synthetic_x, synthetic_y):
-        random_forest_regressor = RandomForestRegressor().fit(synt_x, synth_y)
+        random_forest_regressor = RandomForestRegressor(
+            n_estimators=len(reral_x) // 5,
+        ).fit(synt_x, synth_y)
+        mean_absolute_errors.append(
+            mean_absolute_error(real_y, random_forest_regressor.predict(reral_x))
+        )
+    return mean_absolute_errors
+
+
+def measure_tab_pfn_mean_absolute_error(
+    synthetic_x: list[pd.DataFrame],
+    synthetic_y: list[list[int]],
+    reral_x: pd.DataFrame,
+    real_y: list[int],
+):
+    mean_absolute_errors = []
+    for synt_x, synth_y in zip(synthetic_x, synthetic_y):
+        random_forest_regressor = TabPFNRegressor().fit(synt_x, synth_y)
+        mean_absolute_errors.append(
+            mean_absolute_error(real_y, random_forest_regressor.predict(reral_x))
+        )
+    return mean_absolute_errors
+
+
+def measure_xgb_mean_absolute_error(
+    synthetic_x: list[pd.DataFrame],
+    synthetic_y: list[list[int]],
+    reral_x: pd.DataFrame,
+    real_y: list[int],
+):
+    mean_absolute_errors = []
+    for synt_x, synth_y in zip(synthetic_x, synthetic_y):
+        random_forest_regressor = XGBRegressor().fit(synt_x, synth_y)
         mean_absolute_errors.append(
             mean_absolute_error(real_y, random_forest_regressor.predict(reral_x))
         )
