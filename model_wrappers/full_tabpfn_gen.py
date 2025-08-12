2a5ed25b5e756dfbeef2e6a7387f58200540fd29
mikic202 <mikolaj.chomanski@gmail.com>
2025-08-12 21:26:36 +0200

    Fix small errors to align with the input datatypes

--- model_wrappers/full_tabpfn_gen.py
+++ model_wrappers/full_tabpfn_gen.py
@@ -2,15 +2,14 @@ from tabpfn_extensions import unsupervised
 from tabpfn import TabPFNClassifier, TabPFNRegressor
 import numpy as np
 import pandas as pd
+import torch
 
 
-class FullTabpfnGen(unsupervised.UnsupervisedTabPFN):
-    def __init__(self, *args, **kwargs):
+class FullTabpfnGen(unsupervised.TabPFNUnsupervisedModel):
+    def __init__(self, device):
         super().__init__(
-            args=args,
-            **kwargs,
-            tabpfn_clf=TabPFNClassifier(),
-            tabpfn_reg=TabPFNRegressor()
+            tabpfn_clf=TabPFNClassifier(device=device),
+            tabpfn_reg=TabPFNRegressor(device=device),
         )
 
     def __call__(
@@ -21,15 +20,16 @@ class FullTabpfnGen(unsupervised.UnsupervisedTabPFN):
         attribute_names: list[str] | None = None,
         indices: list[int] | None = None,
         temp=1.0,
+        **kwargs
     ):
-        feature_names = [attribute_names[i] for i in indices]
+        X_train["trarget"] = y_train
+        data = torch.tensor(X_train.to_numpy())
 
-        data = pd.concat([X_train, y_train], axis=1).reindex(X_train.index)
-
-        if indices is not None or attribute_names is not None:
-            categorical_features = data.columns.to_list()
+        if indices is None or attribute_names is None:
+            categorical_features = X_train.columns.to_list()
 
         else:
+            feature_names = [attribute_names[i] for i in indices]
             categorical_features = [
                 feature_names.index(name)
                 for name in attribute_names
