408e07fcc4950850aee9d8fce692b24bdd416a28
mikic202 <mikolaj.chomanski@gmail.com>
2025-10-23 22:53:26 +0200

    Add measuring privacy

--- performance_metrics/measure_privacy.py
+++ performance_metrics/measure_privacy.py
@@ -48,6 +48,24 @@ def calculate_distance_to_nearest_neighbour(
     }
 
 
+def calculate_distance_toNearest_record(
+    synthetic_dataset: pd.DataFrame,
+    real_dataset: pd.DataFrame,
+    identifier_atributes: list[str] | None = None,
+) -> dict[str, float]:
+    if not identifier_atributes:
+        identifier_atributes = synthetic_dataset.columns.tolist()
+
+    model = NearestNeighbors(n_neighbors=1)
+    model.fit(real_dataset[identifier_atributes])
+    distances, _ = model.kneighbors(synthetic_dataset[identifier_atributes])
+    return {
+        "mean": np.mean(distances),
+        "std": np.std(distances),
+        "median": np.median(distances),
+    }
+
+
 def measure_privacy(models, dataset_getter, n_samples: int | None = None):
     train, _ = dataset_getter()
     privacy_results = pd.DataFrame()
@@ -72,6 +90,14 @@ def measure_privacy(models, dataset_getter, n_samples: int | None = None):
         }
         model_privacy_results["k_anonimity"] = calculate_k_anonimity_for_datset(
             synth_data
+        )
+        model_privacy_results.update(
+            {
+                "DCR_" + distance_metric: metric_value
+                for distance_metric, metric_value in calculate_distance_toNearest_record(
+                    synth_data, train
+                ).items()
+            }
         )
         model_privacy_results["model"] = model.__class__.__name__
         privacy_results = pd.concat(
--- test_scenarios/test_privacy.py
+++ test_scenarios/test_privacy.py
@@ -1,6 +1,9 @@
-from external.tab_pfn_gen.src.tabpfgen.tabpfgen import TabPFGen
+from external.tab_pfn_gen.src.tabpfgen.tabpfgen import TabPFGen, TabPFGenClassifier
 from performance_metrics.measure_privacy import measure_privacy
-from test_datasets.dataset_getters import get_climate_model_simulation_dataset
+from test_datasets.dataset_getters import (
+    get_climate_model_simulation_dataset,
+    get_wdbc_dataset,
+)
 import torch
 from model_wrappers.full_tabpfn_gen import FullTabpfnGen
 
@@ -10,13 +13,13 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device:", device)
 
-    generator = TabPFGen(device=str(device))
+    generator = TabPFGenClassifier(device=str(device))
     full_generator = FullTabpfnGen(device=str(device))
 
     print(
         measure_privacy(
-            [generator.generate_classification, full_generator],
-            get_climate_model_simulation_dataset,
+            [generator, full_generator],
+            get_wdbc_dataset,
             1000,
         )
     )
